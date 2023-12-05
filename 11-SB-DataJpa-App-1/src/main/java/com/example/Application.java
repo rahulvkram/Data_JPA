443f88ea817e7a9ba1ad21baaca646f9a1350dec
rahulvkram <gunapatirahul1@gmail.com>
2023-12-04 22:19:41 -0700

    2nd commit

--- 11-SB-DataJpa-App-1/src/main/java/com/example/Application.java
+++ 11-SB-DataJpa-App-1/src/main/java/com/example/Application.java
@@ -12,7 +12,9 @@ import com.example.entity.StudentsEntity;
 import com.example.repository.StudentsRepository;
 
 @SpringBootApplication
-public class Application {
+public class Application { 
+
+
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
